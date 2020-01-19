32bc53ee224c9f60a3c7b418a8edc96a004c3640
unknown <nasreddine.skandrani@gmail.com>
2020-01-19 16:16:45 -0500

    add demo for toast content and toast types

--- super-toastr.js
+++ super-toastr.js
@@ -1,6 +1,6 @@
 var waf = {
-    superToastr: (id_) => {
-        let id = id_;
+    superToastr: (toaster_id_) => {
+        let toaster_id = toaster_id_;
         let counter = 0;
         let toastIds = [];
         let currentToastID = null;
@@ -10,7 +10,117 @@ var waf = {
             success: 0
         };
 
+        let createToastContainer = (toast_id, config) => {
+            var toastElement = document.createElement("DIV");    
+            
+            toastElement.id = toast_id;
+            toastIds.push(toast_id);
+            currentToastID = toast_id;
+            counter++;
+        
+            toastElement.style.position = 'absolute'; 
+            toastElement.style.background = config.background || 'white'; 
+            toastElement.style.border = '1px solid black'; 
+            toastElement.style.padding = '20px'; 
+            toastElement.style.setProperty("-webkit-transition", "top 5s linear");
+            toastElement.style.right = '0';   
+            toastElement.style.zIndex = '102';
+            toastElement.style.maxWidth = '250px';  
+            toastElement.style.minWidth = config.minWidth || '250px';  
+            toastElement.style.margin = '10px';  
+            const res = toastElement.animate([
+                {transform: 'translate(0)'},
+                {transform: 'translate(0, 100px)'}
+            ], 500);
+            res.addEventListener('finish', function() {
+                toastElement.style.transform = 'translate(0, 100px)';
+            });
+
+            return toastElement;
+        };
+
+        let addContent = (toastElement, content) => {
+            toastElement.innerHTML = content;
+        };
+
+        let addCloseBtn = (toastElement, toast_id_) => {
+            var closeElement = document.createElement('div');  
+            closeElement.textContent = 'X';
+            closeElement.style.cursor = 'pointer';
+            closeElement.style.pointerEvents = 'auto';
+            closeElement.style.position = 'absolute';
+            closeElement.style.background = 'white'; 
+            closeElement.style.zIndex = '102';
+            closeElement.style.top = '0';
+            closeElement.style.right = '0';
+            closeElement.style.padding = '3px';
+
+            closeElement.addEventListener('click', function() {
+                const currentToastIndex = toastIds.findIndex(
+                (element) => {
+                    return element === currentToastID;
+                }
+                ); 
+        
+                if (currentToastIndex > 0) {
+                    currentToastID = toastIds[currentToastIndex - 1];
+                } else if (toastIds.length > 1) {
+                    currentToastID = toastIds[currentToastIndex + 1];
+                } else {
+                    currentToastID = null;
+                }
+        
+                if (currentToastID) {
+                    const newNodeToastToRender = document.getElementById(currentToastID + '');
+                    newNodeToastToRender.style.zIndex = '102';
+                }
+        
+                toastIds = toastIds.filter(toastId => toastId !== toast_id_);
+                toastElement.style.display = 'none';
+        
+                self.updateTotalsPerType();
+            }, false);
+            toastElement.appendChild(closeElement); 
+        };
+
         var self = {
+            addToast: (config = {}) => {
+               
+                const __toast_id = config.type + '#' + counter;
+                
+                var toastElement = createToastContainer(__toast_id, config);
+
+                addContent(toastElement, config.content || '');
+
+                if (config.closeBtn) {
+                    addCloseBtn(toastElement, __toast_id);  
+                }
+                
+                document.getElementById('toasts-container').appendChild(toastElement);
+            },
+            
+            /*
+            updateTotalsPerType: () => {
+                total = {
+                    errors: 0,
+                    warnings: 0,
+                    success: 0
+                }
+            
+                toastIds.forEach(toastId => {
+                    if (toastId.includes('ERR#')) {
+                        total.errors++;
+                    }
+                    if (toastId.includes('SUC#')) {
+                        total.success++;
+                    }
+                    if (toastId.includes('WAR#')) {
+                        total.warnings++;
+                    }
+                });
+            },
+            */
+
             nextToast: () => {
                 console.log(' nextToast currentToastID: ', currentToastID);
                 const currentToastIndex = toastIds.findIndex(
@@ -50,116 +160,6 @@ var waf = {
                 }
             
                 console.log(' previousToast END currentToastID: ', currentToastID);
-            },
-            
-            addToast: (type, color) => {
-                var node = document.createElement("DIV");    
-            
-                const __id = type + '#' + counter;
-                node.id = __id;
-                toastIds.push(__id);
-                currentToastID = __id;
-                counter++;
-            
-                node.style.position = 'absolute'; 
-                node.style.background = color; 
-                node.style.border = '1px solid black'; 
-                node.style.padding = '20px'; 
-                node.style.setProperty("-webkit-transition", "top 5s linear");
-                node.style.right = '0';   
-                node.style.zIndex = '102';
-                node.style.maxWidth = '250px';  
-                node.style.margin = '10px';  
-                const res = node.animate([
-                    {transform: 'translate(0)'},
-                    {transform: 'translate(0, 100px)'}
-                ], 500);
-                res.addEventListener('finish', function() {
-                    node.style.transform = 'translate(0, 100px)';
-                });
-            
-            
-                var textnode = document.createTextNode((new Date()).toString());         
-            
-                var Xnode = document.createElement('div');  
-                Xnode.textContent = 'X';
-                Xnode.id = __id;
-                Xnode.style.cursor = 'pointer';
-                Xnode.style.pointerEvents = 'auto';
-                Xnode.style.position = 'absolute';
-                Xnode.style.background = 'white'; 
-                Xnode.style.zIndex = '102';
-                Xnode.style.top = '0';
-                Xnode.style.right = '0';
-                Xnode.style.padding = '3px';
-
-                Xnode.addEventListener('click', function() {
-                    const currentDisplayedNode = document.getElementById(__id + '');
-            
-                    const currentToastIndex = toastIds.findIndex(
-                    (element) => {
-                        return element === currentToastID;
-                    }
-                    ); 
-            
-                    if (currentToastIndex > 0) {
-                        currentToastID = toastIds[currentToastIndex - 1];
-                    } else if (toastIds.length > 1) {
-                        currentToastID = toastIds[currentToastIndex + 1];
-                    } else {
-                        currentToastID = null;
-                    }
-            
-                    if (currentToastID) {
-                        const newNodeToastToRender = document.getElementById(currentToastID + '');
-                        newNodeToastToRender.style.zIndex = '102';
-                    }
-            
-                    toastIds = toastIds.filter(toastId => toastId !== __id);
-                    currentDisplayedNode.style.display = 'none';
-            
-                    self.updateTotalsPerType();
-                }, false);
-                node.appendChild(Xnode); 
-            
-                node.appendChild(textnode);  
-                document.getElementById('toasts-container').appendChild(node);
-            
-                console.log(' addToastWarning currentToastID: ', currentToastID);
-            
-                self.updateTotalsPerType();
-            },
-            
-            addToastWarning: () => {
-                self.addToast('WAR', 'yellow');
-            },
-            
-            addToastSuccess: () => {
-                self.addToast('SUC', '#7FFFD4');
-            },
-            
-            addToastError: () => {
-                self.addToast('ERR', 'red');
-            },
-            
-            updateTotalsPerType: () => {
-                total = {
-                    errors: 0,
-                    warnings: 0,
-                    success: 0
-                }
-            
-                toastIds.forEach(toastId => {
-                    if (toastId.includes('ERR#')) {
-                        total.errors++;
-                    }
-                    if (toastId.includes('SUC#')) {
-                        total.success++;
-                    }
-                    if (toastId.includes('WAR#')) {
-                        total.warnings++;
-                    }
-                });
             }
         }
         return self;
