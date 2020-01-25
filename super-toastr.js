19b1cb92e6f52102802695ddee992d78d613ea91
unknown <nasreddine.skandrani@gmail.com>
2020-01-25 10:00:13 -0500

    add position to demo

--- super-toastr.js
+++ super-toastr.js
@@ -10,6 +10,56 @@ var waf = {
             success: 0
         };
 
+        const positions = {
+            top_right: 'top_right', 
+            bottom_right: 'bottom_right',
+            bottom_left: 'bottom_left',
+            top_left: 'top_left', 
+            top_full_width: 'top_full_width',
+            bottom_full_width: 'bottom_full_width' , 
+            top_center: 'top_center',
+            bottom_center: 'bottom_center'
+        };
+
+        function getTranslation(position, toastElement) {
+            let translate = {};
+            if (position === positions.top_right) {
+                toastElement.style.top = '0'; 
+                toastElement.style.right = '0';  
+                translate = 'translate(0, 100px)';
+            }
+            if (position === positions.bottom_right) {
+                toastElement.style.bottom = '0'; 
+                toastElement.style.right = '0';  
+                translate = 'translate(0, -100px)';
+            }
+            if (position === positions.bottom_left) {
+                toastElement.style.bottom = '0'; 
+                toastElement.style.left = '0';  
+                translate = 'translate(0, -100px)';
+            }
+            if (position === positions.top_left) {
+                toastElement.style.top = '0'; 
+                toastElement.style.left = '0';  
+                translate = 'translate(0, 100px)';
+            }
+            if (position === positions.top_center) {
+                toastElement.style.top = '0'; 
+                toastElement.style.left = '0'; 
+                toastElement.style.right = '0'; 
+                toastElement.style.margin = '0 auto';
+                translate = 'translate(0, 100px)';
+            }
+            if (position === positions.bottom_center) {
+                toastElement.style.bottom = '0'; 
+                toastElement.style.left = '0'; 
+                toastElement.style.right = '0'; 
+                toastElement.style.margin = '0 auto';
+                translate = 'translate(0, -100px)';
+            }
+            return translate;
+        }
+
         let createToastContainer = (toast_id, config) => {
             var toastElement = document.createElement("DIV");    
             
@@ -24,18 +74,19 @@ var waf = {
             toastElement.style.borderRadius = '5px';
             toastElement.style.boxShadow = '0 2px 2px 0 rgba(0,0,0,0.14),0 3px 1px -2px rgba(0,0,0,0.12),0 1px 5px 0 rgba(0,0,0,0.2)';
             toastElement.style.padding = '20px'; 
-            toastElement.style.setProperty("-webkit-transition", "top 5s linear");
-            toastElement.style.right = '0';   
+            toastElement.style.setProperty("-webkit-transition", "top 5s linear"); 
             toastElement.style.zIndex = '102';
             toastElement.style.maxWidth = '250px';  
             toastElement.style.minWidth = config.minWidth || '250px';  
             toastElement.style.margin = '10px';  
+
+            let translate = getTranslation(config.position, toastElement)
             const res = toastElement.animate([
                 {transform: 'translate(0)'},
-                {transform: 'translate(0, 100px)'}
+                {transform: translate}
             ], 500);
             res.addEventListener('finish', function() {
-                toastElement.style.transform = 'translate(0, 100px)';
+                toastElement.style.transform = translate;
             });
 
             return toastElement;
